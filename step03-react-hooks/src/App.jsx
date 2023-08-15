7ffa4986e94137bc5e8261ef3b451d1aa2e3b4b2
Rikito Ikeda <rikito1036@icloud.com>
2023-08-15 20:11:46 +0900

    useMemo

--- step03-react-hooks/src/App.jsx
+++ step03-react-hooks/src/App.jsx
@@ -1,28 +1,31 @@
-import { useReducer } from 'react';
+import { useMemo, useState } from 'react';
 import './App.css';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, 0);
+  const [count01, setCount01] = useState(0);
+  const [count02, setCount02] = useState(0);
+
+  const square = useMemo(() => {
+    let i = 0;
+    while (i < 2000000) {
+      i++;
+    }
+    console.log('クリックされた');
+    return count02 * count02;
+  }, [count02]);
 
   return (
     <div className="App">
-      <h2>useReducer</h2>
-      <p>カウント: {state}</p>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <h2>useMemo</h2>
+      <div>
+        <div>カウント1: {count01}</div>
+        <div>カウント2: {count02}</div>
+        <div>結果: {square}</div>
+        <button onClick={() => setCount01(count01 + 1)}>+</button>
+        <button onClick={() => setCount02(count02 + 1)}>+</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
