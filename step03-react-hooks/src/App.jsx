f425d48e8458bca369898a8031556080116cd4f2
Rikito Ikeda <rikito1036@icloud.com>
2023-08-16 08:43:01 +0900

    カスタムフック調整

--- step03-react-hooks/src/App.jsx
+++ step03-react-hooks/src/App.jsx
@@ -1,29 +1,15 @@
-import { useMemo, useState } from 'react';
+import { useLocalStorage } from 'react';
+import useLocalStorage from './useLocalStorage';
 import './App.css';
 
 const App = () => {
-  const [count01, setCount01] = useState(0);
-  const [count02, setCount02] = useState(0);
-
-  const square = useMemo(() => {
-    let i = 0;
-    while (i < 2000000) {
-      i++;
-    }
-    console.log('クリックされた');
-    return count02 * count02;
-  }, [count02]);
+  const [age, setAge] = useLocalStorage('age', 20);
 
   return (
     <div className="App">
-      <h2>useMemo</h2>
-      <div>
-        <div>カウント1: {count01}</div>
-        <div>カウント2: {count02}</div>
-        <div>結果: {square}</div>
-        <button onClick={() => setCount01(count01 + 1)}>+</button>
-        <button onClick={() => setCount02(count02 + 1)}>+</button>
-      </div>
+      <h2>カスタムフック</h2>
+      <p>{age}</p>
+      <button onClick={() => setAge(25)}>年齢</button>
     </div>
   );
 }
